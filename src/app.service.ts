d70410cdfb56bee7a7dee26835c9395ef40ec8ca
aceiny <a_zeraibi@estin.dz>
2023-12-22 23:58:57 +0100

    super finished

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worldd!';
+    return 'Hello This is a simple nest js task manager created by aceiny , go check other routes';
   }
 }
